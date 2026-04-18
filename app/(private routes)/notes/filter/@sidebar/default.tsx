b2278f17e2b0b309d2c95a38d048ed6d6b120f01
IhorKM <11kozenkoihor@gmail.com>
2026-04-19 00:49:24 +0200

    Fix tree

--- app/(private routes)/notes/filter/@sidebar/default.tsx
+++ app/(private routes)/notes/filter/@sidebar/default.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import css from "./SidebarNotes.module.css";
-import type { NoteTag } from "../../../../types/note";
+import type { NoteTag } from "@/types/note";
 
 const TAGS: NoteTag[] = ["Todo", "Work", "Personal", "Meeting", "Shopping"];
 
