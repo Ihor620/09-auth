871398761075f9137681a253c9e92becfcf5b245
IhorKM <11kozenkoihor@gmail.com>
2026-04-19 02:21:28 +0200

    Fix proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -61,7 +61,7 @@ export async function proxy(request: NextRequest) {
   }
 
   if (isAuthRoute && isAuthenticated) {
-    return NextResponse.redirect(new URL("/profile", request.url));
+  return NextResponse.redirect(new URL("/", request.url));
   }
 
   return response;
